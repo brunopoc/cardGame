090512b310db5a3cee3a082506de983b578e14e4
brunopoc <bruno.poc@gmail.com>
2017-08-08 13:21:19 -0300

    Breve revisão do código (logo um retrabalho em todo o projeto =/)

--- Assets/Scripts/Mechanics/mydeck_scene/next.js
+++ Assets/Scripts/Mechanics/mydeck_scene/next.js
@@ -7,19 +7,26 @@ var firstCheckD : Vector3;
 var tempPosition : Vector3;
 
 function Start ()  {
-	velocidade = 2;
-	cardBehaviour = GameObject.FindGameObjectWithTag("cardBehaviour");
-	deckMachineBehaviour.firstCheckD = cardBehaviour.transform.position;
+	velocidade = 2; // ------------------------------------------------------------------- Velocidade da Transição das cartas (movimentação)
+	cardBehaviour = GameObject.FindGameObjectWithTag("cardBehaviour"); // ---------------- O Objeto pai da cartas que irão se mover
+	deckMachineBehaviour.firstCheckD = cardBehaviour.transform.position; // -------------- Posição inicial das cartas
 }
 
 function Update () {
-	if(checkMouse == true && Input.GetMouseButton(0)){
-		cardBehaviour.transform.position.x += velocidade * Time.deltaTime;
-	}
+	if(checkMouse == true && Input.GetMouseButton(0)){ // -------------------------------- Condição que dispara o evento
+	    moveCard (velocidade, cardBehaviour);
+		controlPosition(cardBehaviour);
 
-	if(checkMouse == true && Input.GetMouseButtonUp(0)){
-					deckMachineBehaviour.distanciaCard = cardBehaviour.transform.position;
-					deckMachineBehaviour.distanciaCard.x = deckMachineBehaviour.firstCheckD.x - cardBehaviour.transform.position.x;
+	}
+}
+
+function moveCard (velocidade : float, objectMoved : GameObject) {
+		objectMoved.transform.position.x += velocidade * Time.deltaTime; // ------------ Movimentação da carta
+}
+
+function controlPosition (objectControl: GameObject) {
+	deckMachineBehaviour.distanciaCard = objectControl.transform.position;
+					deckMachineBehaviour.distanciaCard.x = deckMachineBehaviour.firstCheckD.x - objectControl.transform.position.x;
 			Debug.Log("Na váriavel distanciaCard : " + deckMachineBehaviour.distanciaCard);
 			for(var i: int = 0; i < deckMachineBehaviour.currentDeck.length; i++){
 				if(deckMachineBehaviour.currentDeck[i]!= null){
@@ -32,7 +39,7 @@ function Update () {
 			Debug.Log("Na váriavel tempSpace : " + deckMachineBehaviour.tempSpace);
 		deckMachineBehaviour.firstCheckD = cardBehaviour.transform.position;
 			Debug.Log("Na váriavel firstCheckD : " + deckMachineBehaviour.firstCheckD);
-	}
+
 }
 
 function OnTriggerEnter2D(coll: Collider2D){   
